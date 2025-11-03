879e04b69fdecc855a467a5f16f3d70402ec3db9
Jesús Francisco García Gavilán <gavilanbiost@gmail.com>
2025-11-03 10:16:57 +0100

    Refactor siteConfig properties for clarity
    Updated site configuration by modifying subtitle and description text. Improved text clarity and corrected minor grammatical errors.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -41,8 +41,8 @@ export type SiteConfig = {
 const siteConfig: SiteConfig = {
     website: 'https://gavilanbiost.github.io',
     title: 'Jesús F García Gavilán',
-    subtitle: 'Si hay un patrón, lo encontraré 🔍',
-    description: 'A personal blog and portfolio site built with Astro.js and Tailwind CSS.',
+    /*subtitle: 'Si hay un patrón, lo encontraré 🔍',*/
+    description: 'A personal blog and portfolio site',
     image: {
         src: '',
         alt: ''
@@ -87,16 +87,16 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Bienvenido a mi espacio personal',
-        text: `👋 ¡Hola! Soy Jesús, bioestadístico y científico de datos especializado en el análisis y la interpretación de datos aplicados a la salud y la investigación científica.
-        <p>Desde 2018 he trabajado en el ámbito de la ciencia de datos en salud, una etapa en la que la estadística tradicional convivía con el auge de la bioinformática y el aprendizaje automático. Esta evolución me ha permitido integrar metodologías clásicas y modernas para extraer conocimiento útil a partir de datos complejos y heterogéneos. 
-        </p><p>A lo largo de mi trayectoria, he participado en proyectos que abarcan múltiples tipos de datos: socio-culturales, nutricionales, clínicos, genéticos, metabolómicos, proteómicos y metagenómicos. Mi especialidad es el uso de R para el análisis estadístico, modelado y visualización de datos, aunque también trabajo con Python y SQL, adaptando las herramientas a las necesidades de cada proyecto.
+        text: `👋 ¡Hola! Soy Jesús, bioestadístico y científico de datos especializado en el análisis e interpretación de datos aplicados a la salud e investigación científica.
+        <p>Desde 2018 trabajo en el ámbito de la ciencia de datos en salud, una etapa en la que la estadística tradicional convivía con el auge de la bioinformática y el aprendizaje automático. Esta evolución me ha permitido integrar metodologías clásicas y modernas para extraer conocimiento útil a partir de datos complejos y heterogéneos. 
+        </p><p>A lo largo de mi trayectoria, he participado en proyectos que abarcan múltiples tipos de datos: socioculturales, nutricionales, clínicos, genéticos, metabolómicos, proteómicos y metagenómicos. Mi especialidad es el uso de R para el análisis estadístico, modelado y visualización de datos, aunque también trabajo con Python y SQL, adaptando las herramientas a las necesidades de cada proyecto.
         </p><p> ¿Cuál es mi objetivo?
-        </p><p>Compartir herramientas, código y proyectos que reflejen mi experiencia en bioestadística y ciencia de datos aplicadas a la salud.
+        </p><p>Compartir herramientas, código y proyectos que reflejen mi experiencia en bioestadística y en ciencia de datos aplicadas a la salud.
         </p><p>Busco contribuir al conocimiento colectivo y facilitar el acceso a recursos que ayuden a profesionales, investigadores y estudiantes a resolver problemas reales mediante el uso riguroso y creativo de los datos.
         </p><p> Este espacio es mi manera de aprender, experimentar y colaborar con una comunidad que cree, como yo, que los datos pueden impulsar avances significativos en la investigación, la salud y la toma de decisiones basada en evidencia.`,
         image: {
             src: '/e12ac28f-6872-4b76-9e8a-fe1fd5792af5.jpg',
-            alt: 'Una foto mia en un congreso'
+            alt: 'Una foto mía en un congreso'
         },
         actions: [
             {
